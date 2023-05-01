ad59ba4b8ea8f0db9fb6d9f70e19a58b33d629de
Tanveer Ahmed <ahmed10veer@gmail.com>
2023-05-02 01:51:09 +0600

    spring init

---
+++ src/test/java/com/foodsecurityconsortium/fschome/FschomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodsecurityconsortium.fschome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FschomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
