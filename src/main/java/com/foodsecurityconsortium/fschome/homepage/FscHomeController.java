e269dd4efe2987bda957f294cf20a39cba65c1cd
Tanveer Ahmed <ahmed10veer@gmail.com>
2023-05-05 19:32:48 +0600

    about done

--- src/main/java/com/foodsecurityconsortium/fschome/homepage/FscHomeController.java
+++ src/main/java/com/foodsecurityconsortium/fschome/homepage/FscHomeController.java
@@ -6,18 +6,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FscHomeController {
 
-    @GetMapping("/about")
-    public String about(){
-        return "composition/about";
-    }
-
-//    @GetMapping("/")
     @GetMapping({"/", ""})
     public String home() {
         return "composition/home";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "composition/about";
+    }
+
+    @GetMapping("/whatwedo")
+    public String whatWeDo() {
+        return "composition/what_we_do";
+    }
+
+    @GetMapping("/funding")
+    public String funding() {
+        return "composition/funding";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "composition/blog";
+    }
+
+        @GetMapping("/research")
+    public String research() {
+        return "composition/research";
+    }
+
+        @GetMapping("/howtohelp")
+    public String howToHelp() {
+        return "composition/how_to_help";
+    }
+//    @GetMapping("/donate")
+
+    public String donate() {
+        return "composition/donate";
+    }
+
     @GetMapping("/static")
-    public String hosp(){
+    public String hosp() {
         return "hospital_code/index";
     }
 }
